b5229d780789ef6449c95b3bba00c3bf4dc8c3a3
Divya Patel <113757574+Divya4242@users.noreply.github.com>
2024-03-27 21:08:03 +0530

    Update Hello.js

--- client/src/Hello.js
+++ client/src/Hello.js
@@ -64,15 +64,6 @@ const Hello = () => {
         Sum
       </button>
       <h2>Ans {ans}</h2>
-      //  <button
-      //   type="submit"
-      //   onClick={(e) => {
-      //     sub(e);
-      //   }}
-      //   className="btn btn-primary btn-block mb-3 "
-      // >
-      //   Subtraction
-      // </button>
     </>
   );
 }
