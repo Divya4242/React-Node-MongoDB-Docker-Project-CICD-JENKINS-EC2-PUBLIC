45b971607cded75ac0bef5bfc579f63214e462be
Divya Patel <113757574+Divya4242@users.noreply.github.com>
2024-03-27 21:30:31 +0530

    Update Hello.js

--- client/src/Hello.js
+++ client/src/Hello.js
@@ -21,21 +21,21 @@ const Hello = () => {
     }
   };
 
-  // const sub = async () => {
-  //   try {
-  //     const response = await fetch('http://65.1.248.118:5000/sub',{
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({"num1": Number(num1), "num2": Number(num2)})
-  //     });
-  //     const data = await response.json()
-  //     if(data.result){
-  //       SetAns(data.result)
-  //     }
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // };
+  const sub = async () => {
+    try {
+      const response = await fetch('http://65.1.248.118:5000/sub',{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({"num1": Number(num1), "num2": Number(num2)})
+      });
+      const data = await response.json()
+      if(data.result){
+        SetAns(data.result)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  };
   return (
     <>
       <input
@@ -62,6 +62,15 @@ const Hello = () => {
         className="btn btn-primary btn-block mb-3 "
       >
         Sum
+      </button>
+      <button
+        type="submit"
+        onClick={(e) => {
+          sub(e);
+        }}
+        className="btn btn-primary btn-block mb-3 "
+      >
+        Subtraction
       </button>
       <h2>Ans {ans}</h2>
     </>
