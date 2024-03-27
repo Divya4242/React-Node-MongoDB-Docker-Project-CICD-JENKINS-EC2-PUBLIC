2a89d9c8bbb1fc023b5185416838f64675a6b1d7
Divya Patel <113757574+Divya4242@users.noreply.github.com>
2024-03-27 18:41:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 var cors = require('cors')
 app.use(cors({                                                                                                                 
-    origin: [ 'http://3.110.207.104', 'http://3.110.207.104:3000' ,'*' ]                                                
+    origin: [ 'http://65.1.248.118', 'http://65.1.248.118:3000' ,'*' ]                                                
 }))
 
 app.use(express.json());
