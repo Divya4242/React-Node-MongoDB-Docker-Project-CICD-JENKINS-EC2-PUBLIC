549688c2678d87ba30cb5f0ca4c77ca17beb13d4
Divya Patel <113757574+Divya4242@users.noreply.github.com>
2024-05-15 14:45:36 +0530

    Update Navber.js

--- client/src/components/shop/partials/Navber.js
+++ client/src/components/shop/partials/Navber.js
@@ -73,7 +73,7 @@ const Navber = (props) => {
               style={{ letterSpacing: "0.10rem" }}
               className="flex items-left text-center font-bold uppercase text-gray-800 text-2xl cursor-pointer px-2 text-center"
             >
-              Hayroo
+              Divya
             </span>
           </div>
           <div
@@ -81,7 +81,7 @@ const Navber = (props) => {
             style={{ letterSpacing: "0.70rem" }}
             className="hidden lg:block flex items-left col-span-1 text-center text-gray-800 font-bold tracking-widest uppercase text-2xl cursor-pointer"
           >
-            Hayroo
+            Divya
           </div>
           <div className="flex items-right col-span-2 lg:col-span-1 flex justify-end">
             {/*  WishList Page Button */}
